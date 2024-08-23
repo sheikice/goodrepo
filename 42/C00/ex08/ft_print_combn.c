e4ffbb051b8e32c8956d2725a5186c45975a3990
sheikice <sheikouz@gmail.com>
2024-08-23 16:18:22 +0000

    finish ex08

--- 42/C00/ex08/ft_print_combn.c
+++ 42/C00/ex08/ft_print_combn.c
@@ -1,44 +1,63 @@
 #include <unistd.h>
+#include <stdlib.h>
 
-void	ft_putnbr(int n)
+void	ft_putnbr(int i)
 {
 	char	mod;
 
-	mod = n % 10;	
-	if (n > 0)
-	{
-		ft_putnbr(n / 10);
-	}
+	mod = (i % 10) + 48;	
+	if (i > 9)
+		ft_putnbr(i / 10);
 	write (1, &mod, 1);
+}
+
+void	print_tab(int *tab, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_putnbr(tab[i]);
+		i++;
+	}
+}
+
+void	ft_inc_tab(int *tab, int index, int n)
+{
+	if ((tab[index] + 1) == tab[index + 1])
+	{
+		ft_inc_tab(tab, index - 1, n);
+		tab[index] = tab[index - 1];
+	}
+		tab[index]++;
 }
 
 void	ft_print_combn(int n)
 {
+	int	*tab;
 	int	i;
-	int	limit;
-	int	temp;
 
-	i = 1;
-	limit = 0;
-	while (i <= n)
+	tab = NULL;
+	tab = malloc (sizeof(int) * (n + 1));
+	if (!tab)
+		return;
+	i = 0;
+	while (i < n)
 	{
-		limit = (limit * 10) + 9;
+		tab[i] = i;
 		i++;
 	}
-	i = 1;
-	while (i <= limit)
+	tab[i] = 10;
+	print_tab(tab, n);
+	while (tab[0] < 10 - n)
 	{
-		temp = limit - i;
-		while (temp > 9)
-		{
-			write(1, "0", 1);
-			temp /= 10; 
-		}
-		ft_putnbr(i);
-		if (i < limit - 1)
-			write (1, ", ",1);
-		i++;
+		if(tab[0] < 10 - n)
+			write(1, ", ", 2);
+		ft_inc_tab(tab,  n - 1, n);
+		print_tab(tab, n);
 	}
+	free(tab);	
 }
 
 #include <stdlib.h>
@@ -48,8 +67,5 @@ int	main(int argc, char *argv[])
 		return (0);
 	ft_print_combn(atoi(argv[1]));
 	write (1, "\n", 1);
-	ft_putnbr(0);
-	write (1, "\n", 1);
-	ft_putnbr(1);
 	return (0);
 }
